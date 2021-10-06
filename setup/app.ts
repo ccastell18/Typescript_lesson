a2dd787c3ea18bed23caec19656cae1570c0e6da
Chris Castellano <c.castellano18@gmail.com>
2021-10-05 23:11:17 -0500

    enum

--- setup/app.ts
+++ setup/app.ts
@@ -17,15 +17,35 @@
 
 // };
 
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
+//with role as tuple
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: 'Max',
+//   age: 30,
+//   hobbies: ['sports', 'cooking'],
+//   //a union array. Also a tuple. can 'push' to a tuple, but types must be utilized.
+//   role: [2, 'author'],
+// };
+
+//Enum
+//assigns labels to numbers
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
+
+const person = {
   name: 'Max',
   age: 30,
   hobbies: ['sports', 'cooking'],
-  //a union array. Also a tuple. can 'push' to a tuple, but types must be utilized.
-  role: [2, 'author'],
+  role: Role.ADMIN,
 };
+
+if (person.role === Role.AUTHOR) {
+  console.log('is author');
+}
