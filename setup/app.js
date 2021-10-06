28836b90db1f9983a54dea78e34c1116bfaa1934
Chris Castellano <c.castellano18@gmail.com>
2021-10-05 23:40:39 -0500

    literal types

--- setup/app.js
+++ setup/app.js
@@ -1,45 +1,34 @@
-// This is an object type inferred by Typescript. it creates key: type pairs.
-// const person = {
-//   name: 'Max',
-//   age: 30,
-// };
-//the {} after person is typescript notation of a special object
-// const person: {
-//   name: string;
-//   age: number;
-// } = {
-// const person = {
-//   name: 'Max',
-//   age: 30,
-//   hobbies: ['sports', 'cooking'],
-// };
-//with role as tuple
-// const person: {
-//   name: string;
-//   age: number;
-//   hobbies: string[];
-//   role: [number, string];
-// } = {
-//   name: 'Max',
-//   age: 30,
-//   hobbies: ['sports', 'cooking'],
-//   //a union array. Also a tuple. can 'push' to a tuple, but types must be utilized.
-//   role: [2, 'author'],
-// };
-//Enum
-//assigns labels to numbers
-var Role;
-(function (Role) {
-    Role[Role["ADMIN"] = 0] = "ADMIN";
-    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
-    Role[Role["AUTHOR"] = 2] = "AUTHOR";
-})(Role || (Role = {}));
-var person = {
-    name: 'Max',
-    age: 30,
-    hobbies: ['sports', 'cooking'],
-    role: Role.ADMIN
-};
-if (person.role === Role.AUTHOR) {
-    console.log('is author');
+//Union types
+// function combine(input1: number | string, input2: number | string) {
+//   let result;
+//   if (typeof input1 === 'number' && input2 === 'number') {
+//     result = input1 + input2;
+//   } else {
+//     result = input1.toString() + input2.toString();
+//   }
+//   return result;
+// }
+// const combineAges = combine(30, 26);
+// const combineNames = combine('Max', 'Anna');
+function combine(input1, input2, resultConversion) {
+    var result;
+    if ((typeof input1 === 'number' && input2 === 'number') ||
+        resultConversion === 'as-number') {
+        result = +input1 + +input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    // if (resultConversion === 'as-number') {
+    //   return +result;
+    // } else {
+    //   return result.toString();
+    // }
+    return result;
 }
+var combineAges = combine(30, 26, 'as-number');
+console.log(combineAges);
+var combineStringAges = combine('30', '25', 'as-number');
+console.log(combineStringAges);
+var combineNames = combine('Max', 'Anna', 'as-text');
+console.log(combineNames);
--- setup/app.ts
+++ setup/app.ts
@@ -1,16 +1,51 @@
 //Union types
 
-function combine(input1: number | string, input2: number | string) {
+// function combine(input1: number | string, input2: number | string) {
+//   let result;
+//   if (typeof input1 === 'number' && input2 === 'number') {
+//     result = input1 + input2;
+//   } else {
+//     result = input1.toString() + input2.toString();
+//   }
+
+//   return result;
+// }
+
+// const combineAges = combine(30, 26);
+
+// const combineNames = combine('Max', 'Anna');
+
+function combine(
+  input1: number | string,
+  input2: number | string,
+  //resultConversion: string
+  //literal types
+  resultConversion: 'as-number' | 'as-text'
+) {
   let result;
-  if (typeof input1 === 'number' && input2 === 'number') {
-    result = input1 + input2;
+  if (
+    (typeof input1 === 'number' && input2 === 'number') ||
+    resultConversion === 'as-number'
+  ) {
+    result = +input1 + +input2;
   } else {
     result = input1.toString() + input2.toString();
   }
 
+  // if (resultConversion === 'as-number') {
+  //   return +result;
+  // } else {
+  //   return result.toString();
+  // }
+
   return result;
 }
 
-const combineAges = combine(30, 26);
+const combineAges = combine(30, 26, 'as-number');
+console.log(combineAges);
 
-const combineNames = combine('Max', 'Anna');
+const combineStringAges = combine('30', '25', 'as-number');
+console.log(combineStringAges);
+
+const combineNames = combine('Max', 'Anna', 'as-text');
+console.log(combineNames);
