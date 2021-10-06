b2cd7368ebde9f50701da256a9936b522c1ae95c
Chris Castellano <c.castellano18@gmail.com>
2021-10-05 23:01:13 -0500

    tuple

--- setup/app.ts
+++ setup/app.ts
@@ -5,12 +5,27 @@
 // };
 
 //the {} after person is typescript notation of a special object
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+
+// const person = {
+//   name: 'Max',
+//   age: 30,
+//   hobbies: ['sports', 'cooking'],
+
+// };
+
 const person: {
   name: string;
   age: number;
+  hobbies: string[];
+  role: [number, string];
 } = {
   name: 'Max',
   age: 30,
+  hobbies: ['sports', 'cooking'],
+  //a union array. Also a tuple. can 'push' to a tuple, but types must be utilized.
+  role: [2, 'author'],
 };
-
-console.log(person.name);
